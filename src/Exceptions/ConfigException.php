eee6401af67cf39f12aea3e91973fad028b529e0
xandrw <cosor.alexandru@gmail.com>
2025-02-05 23:18:05 +0200

    Removed redundant constructor

--- src/Exceptions/ConfigException.php
+++ src/Exceptions/ConfigException.php
@@ -4,10 +4,4 @@ namespace Xandrw\ArchitectureEnforcer\Exceptions;
 
 use Exception;
 
-class ConfigException extends Exception
-{
-    public function __construct(string $message)
-    {
-        parent::__construct($message);
-    }
-}
+class ConfigException extends Exception {}
